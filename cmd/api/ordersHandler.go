136edeaae2829c2adeca2528320c281fbc4863e6
bhvalent <benhudvalentine@gmail.com>
2022-09-08 14:15:14 -0500

    updated getOrdersHandler to return json

--- cmd/api/ordersHandler.go
+++ cmd/api/ordersHandler.go
@@ -2,11 +2,37 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type order struct {
+	Items       []item    `json:"items"`
+	CreatedDate time.Time `json:"createdDate"`
+}
+
+type item struct {
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+}
+
 func (app *application) getOrdersHandler(c *gin.Context) {
-	// change this to return data
-	c.String(http.StatusOK, "it worked")
+	orders := []order{
+		{
+			Items: []item{
+				{
+					Description: "yo this is a description",
+					Price:       1.50,
+				},
+				{
+					Description: "this is another description",
+					Price:       3.00,
+				},
+			},
+			CreatedDate: time.Now(),
+		},
+	}
+
+	c.JSON(http.StatusOK, orders)
 }
